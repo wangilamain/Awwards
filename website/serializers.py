d6732e7ba5af27329353c920fa519a76d4092c7a
wangilamain <wangilayng@gmail.com>
2020-11-30 13:04:24 -0500

    added a serializer file

---
+++ website/serializers.py
@@ -0,0 +1,22 @@
+from rest_framework import serializers
+from .models import Profile, Post
+from django.contrib.auth.models import User
+
+class ProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Profile
+        fields = ['fullname', 'profile_picture', 'bio', 'location']
+
+
+class PostSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Post
+        fields = ['id', 'projectname', 'link', 'projectinfo', 'languages', 'picture', 'posted', 'user']
+
+class UserSerializer(serializers.ModelSerializer):
+    profile = ProfileSerializer(read_only=True)
+    posts = PostSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = User
+        fields = ['id', 'url', 'username', 'profile', 'posts']
